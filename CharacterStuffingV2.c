fbb1d288b0b303f708ad5f8c14cc6fa3fe954015
Rishi Vasista <vasistarishi@gmail.com>
2023-01-24 11:34:46 +0530

    Initial Commit

--- CharacterStuffingV2.c
+++ CharacterStuffingV2.c
@@ -42,7 +42,7 @@ void stuffCharacters(char msg[])
             if (count == 3)
             {
                 if(msg[i+1] == 'S' || msg[i+1] == 'E'){
-                    if(msg[i+1] == 'E'){
+                    if(msg[i+1] == 'S'){
                         testFlag = 1;
                     }
                     for(x = i+1; x<6; x++){
@@ -51,11 +51,14 @@ void stuffCharacters(char msg[])
                         }
                     }
                     if(count1 == 3){
-                        if(testFlag){
+                        if(!testFlag){
+                            // i+=3;
+                            // testFlag = 0;
                             printf("DLEETXDLEETX");
                             break;
                         }
                         else{
+                            // i+=3;
                             printf("DLESTXDLESTX");
                             break;
                         }
@@ -80,5 +83,4 @@ void main()
     printf("Enter frame:\n");
     scanf("%s", msg);
     stuffCharacters(msg);
-    
 }

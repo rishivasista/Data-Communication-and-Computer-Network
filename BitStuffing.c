f14b316e986c94cde0bcff25abb4ffa42857f54e
Rishi Vasista <64348057+rishivasista@users.noreply.github.com>
2023-02-23 20:51:26 +0530

    Simplify Code

--- BitStuffing.c
+++ BitStuffing.c
@@ -1,74 +1,70 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
 void stuffBits(char msg[])
 {
-    char newMsg[100] = "", flag0[6] = "00000", flag1[6] = "11111", *temp;
     int i, j, k, x, count0, count1, len;
+    char flag0[6]="00000", flag1[6]="11111", newMsg[100];
     len = strlen(msg);
-    i = 0;
-    x = 5;
+    printf("Before Stuffing : %s\n", msg);
     strcat(newMsg, "00000");
-    while (i < len)
+    x = 5;
+    i=0;
+    while(i<len)
     {
-        j = 0;
-        count0 = 0;
-        count1 = 0;
-        k = 0;
-        while (j < 6)
+        j=0;
+        k=0;
+        count0=0;
+        count1=0;
+        
+        while(j<6)
         {
             newMsg[x++] = msg[i];
-            if (msg[i] == flag0[j] || msg[i] == flag1[k])
+            if(msg[i] == flag0[j] || msg[i] == flag1[j])
             {
-                
-                if (msg[i] == flag0[j])
+                if(msg[i] == flag0[j])
                 {
+                    i++;
                     j++;
                     count0++;
-                    i++;
                 }
                 else
                 {
+                    i++;
                     k++;
                     count1++;
-                    i++;
                 }
-                // i++;
             }
             else
+            i++;
 
-                // i++;
-
-            if (count0 == 5 || count1 == 5)
+            if(count0==5 || count1==5)
             {
-                if (count0 == 5)
+                if(count0==5)
                 {
                     strcat(newMsg, "1");
                     x++;
                     break;
                 }
-                else
-                {
+                else {
                     strcat(newMsg, "0");
                     x++;
                     break;
                 }
-                break;
             }
-            else if ((count0 > 1 && msg[i] != flag0[j]) || (count1 > 1 && msg[i] != flag1[k]))
-            {
-                break;
-            }
+            else if((count0>1 && msg[i]!=flag0[j]) || (count1>1 && msg[i]!=flag1[k]))
+            break;
         }
     }
     strcat(newMsg, "11111");
-    puts(newMsg);
+    
+    printf("After Stuffing : %s\n", newMsg);
 }
 
 int main()
 {
-    char msg[100], *newMsg;
-    printf("Enter a message\n");
+    char msg[100];
+    printf("Enter your message\n");
     scanf("%s", msg);
     stuffBits(msg);
 }
